b0d33297c001a0d6837a7dafeb35f4d589174909
recep-demir <demir.rp@gmail.com>
2025-04-26 22:51:57 +0200

    feat: add user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,37 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true 
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true,
+        set:(password)=>passwordEncrypt(password),
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        validate:[ (email)=>/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ,"Please enter a valid email address" ]
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+
+    },{collection:"users",timestamps:true})
+
+module.exports = mongoose.model("User", UserSchema)
