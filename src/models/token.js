a8a8bfdc51aa61ab5caf98fd4764d6063a5d92e8
recep-demir <demir.rp@gmail.com>
2025-04-27 19:52:56 +0200

    chore: remove commented sections and improve formatting in token model

--- src/models/token.js
+++ src/models/token.js
@@ -1,6 +1,22 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const TokenSchema = new mongoose.Schema({
+
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+        unique:true,
+    },
+    token:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+    }
+
+}, { collection: "tokens" });
+
+module.exports = mongoose.model("Token", TokenSchema);
