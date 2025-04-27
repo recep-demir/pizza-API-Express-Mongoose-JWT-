6b3fbbeec55f9955fc746dcb2684a2d440475b52
recep-demir <demir.rp@gmail.com>
2025-04-27 20:09:50 +0200

    chore: remove unnecessary comments and improve formatting in sync helper

--- src/helpers/sync.js
+++ src/helpers/sync.js
@@ -1,7 +1,6 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+
+
 // sync():
 
 module.exports = async function() {
