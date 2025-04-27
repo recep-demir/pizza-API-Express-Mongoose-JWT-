121a7dfc39efc14c07dcf87f7c05e98687163a07
recep-demir <demir.rp@gmail.com>
2025-04-27 19:50:35 +0200

    chore: remove unnecessary comments and improve code formatting in pizza model

--- src/models/pizza.js
+++ src/models/pizza.js
@@ -1,6 +1,29 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const PizzaSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+      },
+      image:String,
+      price:{
+        type:Number,
+        required:true
+      },
+
+      // Birden fazla topping ( içereceği malzeme(sucuk,mantar gibi) array kullanıldı)
+      toppingIds:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Topping"
+        }
+      ]
+
+
+},{collection:"pizzas",timestamps:true})
+
+module.exports=mongoose.model("Pizza",PizzaSchema)
