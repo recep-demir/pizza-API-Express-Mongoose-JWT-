972b754a4d3f2b6c7161094ffc78c63184589b66
recep-demir <demir.rp@gmail.com>
2025-04-27 19:50:42 +0200

    chore: clean up pizza route file by removing commented sections and improving formatting

--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -1,9 +1,14 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+const router = require('express').Router()
+const pizza=require("../controllers/pizza")
+
+
+router.route("/").get(pizza.list).post(pizza.create)
+
+router.route("/:id")
+.get(pizza.read)
+.put(pizza.update)
+.patch(pizza.update)
+.delete(pizza.delete)
 module.exports = router
