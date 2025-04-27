f159f4de3edf3be10d1fd449c44c2b59b55866d1
recep-demir <demir.rp@gmail.com>
2025-04-27 16:23:38 +0200

    chore: refactor order model schema for improved readability and structure

--- src/models/order.js
+++ src/models/order.js
@@ -1,6 +1,45 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const OrderSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true,
+      },
+    pizzaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Pizza",
+        required: true,
+        unique: true,
+      },
+      size:{
+        type:String,
+        trim:true,
+        required:true,
+        // belli bir ölçülerle sınırlandırma için enum kodunu kullanıyoruz.
+        enum:["Small","Medium","Large","XLarge"]
+    
+      },
+    quantity:{
+        type:Number,
+        default:1
+      },
+    price:{
+        type:Number,
+        required:true
+      },
+    totalPrice:{
+      type:Number,
+      default:function(){
+        return this.quantity*this.price
+      }
+    }
+
+
+},{collation:"order",timestamps:true})
+
+module.exports=mongoose.model("Order",OrderSchema)
