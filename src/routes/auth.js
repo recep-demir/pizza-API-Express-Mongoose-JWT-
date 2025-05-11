4a20ab3d103b03b3c35bdffba505830892bd8db2
recep-demir <demir.rp@gmail.com>
2025-05-11 22:22:23 +0200

    chore: reorganize and clarify structure of auth.js route file

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 "use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+const router = require('express').Router()
+const { login, logout, refresh } = require('../controllers/auth');
+
+// URL: /auth
+
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/refresh', refresh);
+
 module.exports = router
