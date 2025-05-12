fcaede97ebb14cae9aaa37db69b284aaf176ec16
recep-demir <demir.rp@gmail.com>
2025-05-12 18:27:15 +0200

    fix: refactor authentication middleware to handle token verification and user assignment

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,2 +1,25 @@
 "use strict"
 
+const Token = require('../models/token');
+const jwt = require('jsonwebtoken')
+
+module.exports = async (req, res, next) => {
+     req.user = null;
+     const auth = req.headers?.authorization; 
+     const tokenArr = auth ? auth.split(' ') : null;
+
+     if (tokenArr && tokenArr[0] == 'Token') {
+        const tokenData = await Token.findOne({ token: tokenArr[1] }).populate('userId') // {userId : {...userInfo}, token} || null
+        req.user = tokenData ? tokenData.userId : null;
+
+    } else if (tokenArr && tokenArr[0] == 'Bearer') {
+        // jwt.verify(jwtToken, secretKey, cb )
+        jwt.verify(tokenArr[1], process.env.ACCESS_KEY, (error, accessData) => {
+            // console.log(error);
+            // console.log(accessData);
+            req.user = accessData ? accessData : null;
+        });
+    }
+
+    next();
+}
