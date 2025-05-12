5ccaef32a7bd4fc99f3c5db4bcee1385ac0a9c39
recep-demir <demir.rp@gmail.com>
2025-05-12 15:35:31 +0200

    chore: comment out unused imports for clarity in auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,10 @@
 "use strict"
 
-const CustomError = require("../helpers/customError");
+// const CustomError = require("../helpers/customError");
 const User = require('../models/user');
 const Token = require('../models/token');
 const passwordEncrypt = require('../helpers/passwordEncrypt');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 
 module.exports = {
