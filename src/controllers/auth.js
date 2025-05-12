017766d6e8ad0a0f287fb487233c09888fa47159
recep-demir <demir.rp@gmail.com>
2025-05-12 16:08:00 +0200

    fix: enhance error handling in authentication logic and clean up response structure

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,25 +24,20 @@ module.exports = {
         */
 
         const { username, email, password } = req.body;
-
-        if (!((username || email) && password)) throw new CustomError('username/email and password are required', 401);
-
-        const user = await User.findOne({ $or: [{ email }, { username }], password });
+        if (!((username||email)&& password)) throw new CustomError('username/email and password are required',401);
+        const user = await User.findOne({$or:[{username}||{email}],password})
 
         if (!user) throw new CustomError('Incorrect email/username or password', 401);
-
         if (!user.isActive) throw new CustomError('This account is not active.');
 
         /* Simple Token */
         let tokenData = await Token.findOne({ userId: user._id });
-
         if (!tokenData) {
             tokenData = await Token.create({
                 userId: user._id,
                 token: passwordEncrypt(Date.now() + user._id)
             });
         };
-        /* Simple Token */
 
         /* JWT Token */
         // Access Token:
@@ -52,23 +47,19 @@ module.exports = {
             isActive: user.isActive,
             isAdmin: user.isAdmin
         };
-
         // const accessToken = jwt.sign(payload, secretKey, {lifetime})
         const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '15m' });
 
         // Refresh Token:
         const refreshToken = jwt.sign({ _id: user._id }, process.env.REFRESH_KEY, { expiresIn: '1d' });
-        /* JWT Token */
 
-        res.status(200).send({
-            error: false,
-            bearer: {
-                access: accessToken,
-                refresh: refreshToken
-            },
-            token: tokenData.token,
-            user: user
-        })
+
+
+
+
+
+
+       
     },
 
     logout: async (req, res) => {
