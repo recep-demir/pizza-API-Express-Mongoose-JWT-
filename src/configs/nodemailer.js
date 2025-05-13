d6ed98f3afad97ce4993ccfd4e0494178bef5025
recep-demir <demir.rp@gmail.com>
2025-05-13 21:14:08 +0200

    feat: add nodemailer configuration for email handling

---
+++ src/configs/nodemailer.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const nodemailer = require('nodemailer');
+
+module.exports = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.ADMIN_EMAIL,
+        pass: process.env.EMAIL_PASS,
+    }
+});
