ea54ec983ba3bbea9eae505a38d20c69cbadc7d1
recep-demir <demir.rp@gmail.com>
2025-05-15 17:02:08 +0200

    feat: integrate upload middleware for image handling in pizza routes

--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -2,15 +2,17 @@
 
 const router = require('express').Router()
 const pizza=require("../controllers/pizza")
-// const upload = require('../middlewares/upload');
+const upload = require('../middlewares/upload');
 
 
 
-router.route("/").get(pizza.list).post(pizza.create)
+router.route("/")
+.get(pizza.list)
+.post(upload.single('image'), pizza.create)
 
 router.route("/:id")
 .get(pizza.read)
-.put(pizza.update)
-.patch(pizza.update)
+.put(upload.single('image'),pizza.update)
+.patch(upload.single('image'),pizza.update)
 .delete(pizza.delete)
 module.exports = router
