98acf9734b12d11acd36be4c223e2032ec0201ad
recep-demir <demir.rp@gmail.com>
2025-05-15 15:23:49 +0200

    feat: implement storage configuration for multer in upload middleware

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -3,5 +3,11 @@
 const multer = require('multer');
 
 module.exports = multer({
+        storage: multer.diskStorage({
+        destination: './uploads',
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + '_' + file.originalname)
+        }
+    })
     
 })
